9244ea9a2b910aa1ef1212d823f520f49467e2f3
JunJacobYoung <1285393364@qq.com>
2023-10-06 00:36:23 +0800

    Init

---
+++ Scripts/ScriptsPractice/HelloWorld.cs
@@ -0,0 +1,25 @@
+﻿using DynamicPatcher;
+using Extension.Ext;
+using Extension.Script;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DionysusPractice.DynamicPatcher.Scripts.ScriptsPractice
+{
+    [Serializable]
+    public class HelloWorld : TechnoScriptable
+    {
+        public HelloWorld(TechnoExt owner) : base(owner) { }
+
+        int count = 0;
+
+        public override void OnUpdate()
+        {
+            Logger.Log($"HelloWorld {count++}");
+            base.OnUpdate();
+        }
+    }
+}
